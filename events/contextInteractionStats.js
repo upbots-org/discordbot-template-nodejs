e87dd77dca7a5f540156f5742c0d9af6efacf3d7
Luamix <96681582+Luamix-dev@users.noreply.github.com>
2023-04-08 20:13:20 +0200

    added usercontext menu stats

--- events/contextInteractionStats.js
+++ events/contextInteractionStats.js
@@ -47,6 +47,391 @@ module.exports = {
             // A try to execute the interaction.
 
             if (!command) return;
+
+            const color = require('../configurations/colors');
+            const logs = require('../configurations/logs');
+            const avatar = require('../configurations/avatars');
+
+            let d = new Date();
+            d.setHours(0, 0, 0, 0);
+
+            // A try to executes the interaction.
+
+            const ChannelUserContextStats = require('../models/stats/ChannelUserContextStats');
+            const DmUserContextStats = require('../models/stats/DmUserContextStats');
+            const GlobalUserContextStats = require('../models/stats/GlobalUserContextStats');
+            const GuildUserContextStats = require('../models/stats/GuildUserContextStats');
+            const GroupDmUserContextStats = require('../models/stats/GroupDmUserContextStats');
+
+            if (interaction?.channel?.type == ChannelType.DM) {
+                const dataDmUserContextStats = await DmUserContextStats.findOne({
+                    date: d,
+                    userId: interaction.user.id,
+                    dmChannelId: interaction.channel.id
+                });
+
+                if (dataDmUserContextStats) {
+                    if (dataDmUserContextStats.usercontexts.find((x) => x.id == interaction.commandName)) {
+                        dataDmUserContextStats.usercontexts.map((x) => {
+                            if (x.id == interaction.commandName) x.count += 1;
+                            return x;
+                        });
+                    } else {
+                        dataDmUserContextStats.usercontexts.push({ id: interaction.commandName, count: 1 });
+                    }
+
+                    await dataDmUserContextStats.save();
+                } else {
+                    await DmUserContextStats.create({
+                        usercontexts: [{ id: interaction.commandName, count: 1 }],
+                        dmChannelId: interaction.channel.id,
+                        userId: interaction.user.id,
+                        date: d
+                    });
+                }
+            }
+
+            // GroupDmUserContextStats
+
+            if (interaction?.channel?.type == ChannelType.GroupDM) {
+                const dataGroupDmUserContextStats = await GroupDmUserContextStats.findOne({
+                    date: d,
+                    userId: interaction.user.id,
+                    dmChannelId: interaction.channel.id
+                });
+
+                if (dataGroupDmUserContextStats) {
+                    if (dataGroupDmUserContextStats.usercontexts.find((x) => x.id == interaction.commandName)) {
+                        dataGroupDmUserContextStats.usercontexts.map((x) => {
+                            if (x.id == interaction.commandName) x.count += 1;
+                            return x;
+                        });
+                    } else {
+                        dataGroupDmUserContextStats.usercontexts.push({ id: interaction.commandName, count: 1 });
+                    }
+
+                    await dataGroupDmUserContextStats.save();
+                } else {
+                    await GroupDmUserContextStats.create({
+                        usercontexts: [{ id: interaction.commandName, count: 1 }],
+                        dmChannelId: interaction.channel.id,
+                        userId: interaction.user.id,
+                        date: d
+                    });
+                }
+            }
+
+            // ChannelUserContextStats
+
+            if (interaction?.channel?.type != ChannelType.DM && interaction?.channel?.type != ChannelType.GroupDM) {
+                const dataChannelUserContextStats = await ChannelUserContextStats.findOne({
+                    date: d,
+                    userId: interaction.user.id,
+                    channelId: interaction.channel.id,
+                    guildId: interaction.guild.id
+                });
+
+                if (dataChannelUserContextStats) {
+                    if (dataChannelUserContextStats.usercontexts.find((x) => x.id == interaction.commandName)) {
+                        dataChannelUserContextStats.usercontexts.map((x) => {
+                            if (x.id == interaction.commandName) x.count += 1;
+                            return x;
+                        });
+                    } else {
+                        dataChannelUserContextStats.usercontexts.push({ id: interaction.commandName, count: 1 });
+                    }
+
+                    await dataChannelUserContextStats.save();
+                } else {
+                    await ChannelUserContextStats.create({
+                        usercontexts: [{ id: interaction.commandName, count: 1 }],
+                        channelId: interaction.channel.id,
+                        guildId: interaction.guild.id,
+                        userId: interaction.user.id,
+                        date: d
+                    });
+                }
+            }
+
+            // GuildUserContextStats
+
+            if (interaction?.guild) {
+                const dataGuildUserContextStats = await GuildUserContextStats.findOne({
+                    date: d,
+                    userId: interaction.user.id,
+                    guildId: interaction.guild.id
+                });
+
+                if (dataGuildUserContextStats) {
+                    if (dataGuildUserContextStats.usercontexts.find((x) => x.id == interaction.commandName)) {
+                        dataGuildUserContextStats.usercontexts.map((x) => {
+                            if (x.id == interaction.commandName) x.count += 1;
+                            return x;
+                        });
+                    } else {
+                        dataGuildUserContextStats.usercontexts.push({ id: interaction.commandName, count: 1 });
+                    }
+
+                    await dataGuildUserContextStats.save();
+                } else {
+                    await GuildUserContextStats.create({
+                        usercontexts: [{ id: interaction.commandName, count: 1 }],
+                        guildId: interaction.guild.id,
+                        userId: interaction.user.id,
+                        date: d
+                    });
+                }
+            }
+
+            // GlobalUserContextStats
+
+            const dataGlobalUserContextStats = await GlobalUserContextStats.findOne({
+                date: d,
+                userId: interaction.user.id
+            });
+
+            if (dataGlobalUserContextStats) {
+                if (dataGlobalUserContextStats.usercontexts.find((x) => x.id == interaction.commandName)) {
+                    dataGlobalUserContextStats.usercontexts.map((x) => {
+                        if (x.id == interaction.commandName) x.count += 1;
+                        return x;
+                    });
+                } else {
+                    dataGlobalUserContextStats.usercontexts.push({ id: interaction.commandName, count: 1 });
+                }
+
+                await dataGlobalUserContextStats.save();
+            } else {
+                await GlobalUserContextStats.create({
+                    usercontexts: [{ id: interaction.commandName, count: 1 }],
+                    userId: interaction.user.id,
+                    date: d
+                });
+            }
+
+            // Logs
+
+            const forumChannel = client.channels.cache.get(logs.interactionsForumChannelId);
+
+            if (!forumChannel) return client.out.alert('No forumChannel found', this.name);
+
+            const webhooks = await forumChannel.fetchWebhooks();
+
+            let webhook = null;
+
+            if (webhooks.find((x) => x.name == 'UserContext')) {
+                webhook = new WebhookClient({ url: webhooks.find((x) => x.name == 'UserContext').url });
+            } else {
+                webhook = await forumChannel.createWebhook({ name: 'UserContext', avatar: avatar.usercontext });
+            }
+
+            // This UserContext's stats in every guild (total time)
+
+            const dataGlobalUserContextCount = await GlobalUserContextStats.find({});
+
+            let a = 0;
+
+            dataGlobalUserContextCount.forEach((x) => {
+                if (x.usercontexts.find((x) => x.id === interaction.commandName)) {
+                    a += x.usercontexts.find((x) => x.id === interaction.commandName).count;
+                }
+            });
+
+            const dataGlobalUserContextCountToday = await GlobalUserContextStats.find({ date: d });
+
+            let e = 0;
+
+            dataGlobalUserContextCountToday.forEach((x) => {
+                if (x.usercontexts.find((x) => x.id === interaction.commandName)) {
+                    e += x.usercontexts.find((x) => x.id === interaction.commandName).count;
+                }
+            });
+
+            let webhookGuilds = null;
+
+            if (interaction.guild) {
+                // This UserContext's stats in this guild (total time)
+
+                const dataGuildUserContextCount = await GuildUserContextStats.find({ guildId: interaction.guild.id });
+
+                let b = 0;
+
+                dataGuildUserContextCount.forEach((x) => {
+                    if (x.usercontexts.find((x) => x.id === interaction.commandName)) {
+                        b += x.usercontexts.find((x) => x.id === interaction.commandName).count;
+                    }
+                });
+
+                const dataGuildUserContextCountTodaay = await GuildUserContextStats.find({ guildId: interaction.guild.id, date: d });
+
+                let f = 0;
+
+                dataGuildUserContextCountTodaay.forEach((x) => {
+                    if (x.usercontexts.find((x) => x.id === interaction.commandName)) {
+                        f += x.usercontexts.find((x) => x.id === interaction.commandName).count;
+                    }
+                });
+
+                // Fetching GuildSettings
+
+                const GuildSettings = require('../models/guilds/GuildSettings');
+
+                let dataGuildSettings = null;
+
+                dataGuildSettings = await GuildSettings.findOne({ id: interaction.guild.id });
+
+                if (!dataGuildSettings) {
+                    client.out.alert('No dataGuildSettings', this.name);
+                }
+
+                // Sending Logs
+
+                webhook
+                    .send({
+                        embeds: [
+                            new EmbedBuilder()
+                                .setColor(color.usercontext)
+                                .setFooter({
+                                    text: client.configs.footer.defaultText,
+                                    iconURL: client.configs.footer.displayIcon ? client.configs.footer.defaultIcon : ''
+                                })
+                                .setDescription(
+                                    `> __**Informations**__\n→ Guild: **${interaction.guild.name}** (||${interaction.guild.id}||) [${
+                                        interaction.guild.memberCount
+                                    } Members - <t:${parseInt(interaction.guild.createdTimestamp / 1000)}:R>]\n→ Language: **${
+                                        dataGuildSettings.language
+                                    }**\n→ Channel: **#${interaction.channel.name}** (||${interaction.channel.id}||) [<t:${parseInt(
+                                        interaction.channel.createdTimestamp / 1000
+                                    )}:R>]\n→ User: **${interaction.user.tag}** (||${interaction.user.id}||) [<t:${parseInt(
+                                        interaction.user.createdTimestamp / 1000
+                                    )}:R>]\n→ Target-User: **${client.users.cache.get(interaction.targetId).tag}** (||${
+                                        interaction.targetId
+                                    }||) [<t:${parseInt(
+                                        client.users.cache.get(interaction.targetId).createdAt / 1000
+                                    )}:R>]\n\n> __**Usage-data**__\n→ UserContext: **${interaction.commandName}** (||${
+                                        interaction.commandName
+                                    }||)\n→ Today [Guild / Global  / On this guild]: **${f} / ${e} (${parseInt(
+                                        (f / e) * 100
+                                    )}%)**\n→ All-Time [Guild / Global / On this guild]: **${b} / ${a} (${parseInt((b / a) * 100)}%)**`
+                                )
+                                .setTimestamp()
+                        ],
+                        threadId: client.configs.logs.usercontextThreadId
+                    })
+                    .catch((err) => {
+                        client.out.warn('Error with LogWebhookUrl (Sending) ' + this.name);
+
+                        client.out.error(err);
+                    });
+
+                const forumChannelGuilds = client.channels.cache.get(logs.guildsForumChannelId);
+
+                if (!forumChannelGuilds) return client.out.alert('No forumChannelGuilds found', this.name);
+
+                const webhooksGuildss = await forumChannelGuilds.fetchWebhooks();
+
+                if (webhooksGuildss.find((x) => x.name == 'UserContext')) {
+                    webhookGuilds = new WebhookClient({ url: webhooksGuildss.find((x) => x.name == 'UserContext').url });
+                } else {
+                    webhookGuilds = await forumChannelGuilds.createWebhook({ name: 'UserContext', avatar: avatar.usercontext });
+                }
+
+                if (webhookGuilds) {
+                    webhookGuilds
+                        .send({
+                            embeds: [
+                                new EmbedBuilder()
+                                    .setColor(color.usercontext)
+                                    .setFooter({
+                                        text: client.configs.footer.defaultText,
+                                        iconURL: client.configs.footer.displayIcon ? client.configs.footer.defaultIcon : ''
+                                    })
+                                    .setDescription(
+                                        `> __**Informations**__\n→ Guild: **${interaction.guild.name}** (||${interaction.guild.id}||) [${
+                                            interaction.guild.memberCount
+                                        } Members - <t:${parseInt(interaction.guild.createdTimestamp / 1000)}:R>]\n→ Language: **${
+                                            dataGuildSettings.language
+                                        }**\n→ Channel: **#${interaction.channel.name}** (||${interaction.channel.id}||) [<t:${parseInt(
+                                            interaction.channel.createdTimestamp / 1000
+                                        )}:R>]\n→ User: **${interaction.user.tag}** (||${interaction.user.id}||) [<t:${parseInt(
+                                            interaction.user.createdTimestamp / 1000
+                                        )}:R>]\n→ Target-User: **${client.users.cache.get(interaction.targetId).tag}** (||${
+                                            interaction.targetId
+                                        }||) [<t:${parseInt(
+                                            client.users.cache.get(interaction.targetId).createdAt / 1000
+                                        )}:R>]\n\n> __**Usage-data**__\n→ UserContext: **${interaction.commandName}** (||${
+                                            interaction.commandName
+                                        }||)\n→ Today [Guild / Global  / On this guild]: **${f} / ${e} (${parseInt(
+                                            (f / e) * 100
+                                        )}%)**\n→ All-Time [Guild / Global / On this guild]: **${b} / ${a} (${parseInt((b / a) * 100)}%)**`
+                                    )
+                                    .setTimestamp()
+                            ],
+                            threadId: dataGuildSettings.threadId
+                        })
+                        .catch((err) => {
+                            client.out.warn('Error with LogWebhookUrlGuilds (Sending) ' + this.name);
+
+                            client.out.error(err);
+                        });
+                } else client.out.alert('No webhookGuilds', this.name);
+            } else {
+                // This usercontext's stats in this DM (total time)
+
+                const dataDmUserContextCount = await DmUserContextStats.find({ dmChannelId: interaction.channel.id });
+
+                let c = 0;
+
+                dataDmUserContextCount.forEach((x) => {
+                    if (x.usercontexts.find((x) => x.id === interaction.commandName)) {
+                        c += x.usercontexts.find((x) => x.id === interaction.commandName).count;
+                    }
+                });
+
+                const dataDmUserContextCountToday = await DmUserContextStats.find({ dmChannelId: interaction.channel.id });
+
+                let g = 0;
+
+                dataDmUserContextCountToday.forEach((x) => {
+                    if (x.usercontexts.find((x) => x.id === interaction.commandName)) {
+                        g += x.usercontexts.find((x) => x.id === interaction.commandName).count;
+                    }
+                });
+
+                webhook
+                    .send({
+                        embeds: [
+                            new EmbedBuilder()
+                                .setColor(color.usercontext)
+                                .setFooter({
+                                    text: client.configs.footer.defaultText,
+                                    iconURL: client.configs.footer.displayIcon ? client.configs.footer.defaultIcon : ''
+                                })
+                                .setDescription(
+                                    `> __**Informations**__\n→ Channel: **DM** (||${interaction.channel.id}||) [<t:${parseInt(
+                                        interaction.channel.createdTimestamp / 1000
+                                    )}:R>]\n→ User: **${interaction.user.tag}** (||${interaction.user.id}||) [<t:${parseInt(
+                                        interaction.user.createdTimestamp / 1000
+                                    )}:R>]\n→ Target-User: **${client.users.cache.get(interaction.targetId).tag}** (||${
+                                        interaction.targetId
+                                    }||) [<t:${parseInt(
+                                        client.users.cache.get(interaction.targetId).createdAt / 1000
+                                    )}:R>]\n\n> __**Usage-data**__\n→ UserContext: **${interaction.commandName}** (||${
+                                        interaction.commandName
+                                    }||)\n→ Today [This DM / Global  / On this DM]: **${g} / ${e} (${parseInt(
+                                        (g / e) * 100
+                                    )}%)**\n→ All-Time [This DM / Global / On this DM]: **${c} / ${a} (${parseInt((c / a) * 100)}%)**`
+                                )
+                                .setTimestamp()
+                        ],
+                        threadId: client.configs.logs.usercontextThreadId
+                    })
+                    .catch((err) => {
+                        client.out.warn('Error with LogWebhookUrl (Sending) ' + this.name);
+
+                        client.out.error(err);
+                    });
+            }
         }
         // Checks if the interaction target was a user
         else if (interaction.isMessageContextMenuCommand()) {
